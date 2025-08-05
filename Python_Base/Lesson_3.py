e21dea63f221bfec8b5c22a87e14188b6c404e4d
ner <ivanmironoved@gmail.com>
2025-08-05 19:39:00 +0200

    Типы данных в языке программирования Python.

--- Python_Base/Lesson_3.py
+++ Python_Base/Lesson_3.py
@@ -2,3 +2,15 @@
 num_1 = int(input('Input first number: '))
 num_2 = int(input('Input second number: '))
 print('Answer:', num_1 + num_2)
+
+print('-' * 15)
+print('First practical work:')
+my_str = "175"
+my_float = 15.5
+print(float(my_str) + my_float)
+print(my_str + str(my_float))
+
+print('-' * 15)
+print('Second practical work:')
+User_input = input('Пожалуйста, введите своё имя: ')
+print('Привет,', User_input)
