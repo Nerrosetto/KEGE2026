41d69bf85fb844f4e29584ddf7005e1cb576e935
ner <ivanmironoved@gmail.com>
2025-08-05 18:39:15 +0200

    Типы данных в языке программирования Python.

---
+++ Python_Base/Lesson_2.py
@@ -0,0 +1,9 @@
+# Таблица перевода типов данных.
+# int -> bool(), float(), str()
+# float -> int(), str(), bool()
+# str -> int(), float(), bool(), list(), tuple(), set()
+# list -> tuple(), set(), str(), bool()
+# tuple -> list(), set(), str(), bool()
+# set -> tuple(), list(), str(), bool()
+# dict -> str(), bool(), list(), tuple(), set()
+# bool -> int(), str(), float()
