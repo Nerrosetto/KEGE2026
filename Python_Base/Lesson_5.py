824002d2ec382bbb6183eb585cb7f182d2a35631
ner <ivanmironoved@gmail.com>
2025-08-08 19:32:01 +0200

    Работа с библиотеками.

--- Python_Base/Lesson_5.py
+++ Python_Base/Lesson_5.py
@@ -7,11 +7,15 @@
 #
 # # подключение одной команды из библиотеки
 # from random import randint as hello
+#
 # # подключение нескольких команд из библиотеки
 # from random import randint, choice
+#
 # # Подключение всех команд из библиотеки
 # from random import *
+#
 # # Подключение одной команды из библиотеки как псевдоним
 # from random import randint as dp
+#
 # # Подключение нескольких команд из библиотеки через псевдоним
 # from random import randint as dp, choice as pl
