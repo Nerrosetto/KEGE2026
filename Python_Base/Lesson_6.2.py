5db6c865ca3972085cdb67fe0f18c487ac0403a2
ner <ivanmironoved@gmail.com>
2025-08-12 18:52:47 +0200

    Задания (практика).

---
+++ Python_Base/Lesson_6.2.py
@@ -0,0 +1,4 @@
+num1 = int(input('Введите трёхзначное число: '))
+print(num1 // 100)
+print(num1 // 10 % 10)
+print(num1 % 10)
---
+++ Python_Base/Lesson_6.3.py
@@ -0,0 +1,6 @@
+num1 = int(input())
+num2 = int(input())
+num1 *= 6
+num1 //= 4
+num2 += num1 % 5
+print(num1, num2)
