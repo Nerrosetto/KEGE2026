e462fbd277150cc106b0c9a847ad1dc60486dd3e
ner <ivanmironoved@gmail.com>
2025-08-22 19:22:56 +0200

    Занятие 8.

--- Python_Base/Lesson_8.py
+++ Python_Base/Lesson_8.py
@@ -6,3 +6,17 @@
 # # continue - переходит к следующему шагу цикла.
 # # break - останавливает работу цикла
 # # else - выполняет блок кода если цикл завершился без break
+
+# num = int(input())
+# while num:
+#     print(num % 10)
+#     num //= 10
+
+num = int(input())
+num2 = []
+while num >= 1:
+    num2.append(num % 10)
+    num //= 10
+while num2:
+    print(num2[-1])
+    num2.remove(num2[-1])
