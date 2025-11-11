9d60d660eda682fc467307cd6c57de53f5e10daf
ner <ivanmironoved@gmail.com>
2025-11-11 19:51:34 +0200

    Теория задания типа 5

--- Task5/Theory.py
+++ Task5/Theory.py
@@ -17,6 +17,7 @@ print(f'{N:o}')
 print(hex(N)[2:])
 print(f'{N:x}')
 
+
 # Перевод в любую систему (2 <= sys <= 9) sys- system
 def convert(num, sys):
     res = ''
@@ -25,10 +26,12 @@ def convert(num, sys):
         num //= sys
     return res[::-1]
 
+
 print(convert(N, 3))
 
 # Перевод в любую систему (2 <= sys <= 36)
 from string import printable as al
+
 
 def convert2(num, sys):
     res = ''
@@ -36,6 +39,7 @@ def convert2(num, sys):
         res += al[num % sys]
         num //= sys
     return res[::-1]
+
 
 # Перевод в десятичную систему.
 num_bin = '101'
@@ -45,3 +49,31 @@ num_hex = 'fa8'
 print(int(num_bin, 2))
 print(int(num_tri, 3))
 print(int(num_hex, 16))
+
+# Срезы.
+data = "123456789"
+# Извлечение первых двух символов.
+print(data[:2])
+# Извлечение без первых двух символов.
+print(data[2:])
+# Извлечение последних двух символов.
+print(data[-2:])
+# Извлечение без последних двух символов.
+print(data[:-2])
+
+# Сумма цифр числа.
+# Двоичная система.
+num1 = '1010'
+sum1 = num1.count('1')
+
+# Любая система до десятой включительно.
+num2 = '193'
+sum2 = sum(map(int, num2))
+print(sum2)
+
+a = [*map(int, num2)]
+print(a)
+
+# Любая система до 36 включительно.
+num3 = 'AF5'
+sum3 = sum(map(lambda x: int(x, 36), num3))
