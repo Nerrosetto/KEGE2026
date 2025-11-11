36cc753b67025edf79d6b3e6bf0f085949ed7cdb
ner <ivanmironoved@gmail.com>
2025-11-11 19:52:03 +0200

    Задания типа 5

--- Task5/9774.py
+++ Task5/9774.py
@@ -8,6 +8,7 @@ def convert(num, sys):
         num //= sys
     return res[::-1]
 
+
 ans = []
 for N in range(1, 5000):
     R = convert(N, 3)
@@ -16,5 +17,5 @@ for N in range(1, 5000):
     else:
         R += convert(N % 3 * 5, 3)
     if int(R, 3) > 133:
-        ans.append(int(R,3))
+        ans.append(int(R, 3))
 print(min(ans))
