70c850cad705d238da2703826d500955b1c2c403
ner <ivanmironoved@gmail.com>
2025-11-25 19:18:17 +0200

    Д/З 24112025

--- Homework/9918.py
+++ Homework/9918.py
@@ -1,8 +1,7 @@
-con = 0
-res = []
+res = set()
 for x in range(10, 67):
     for y in range(0, x):
         num1 = 7 * 67 ** 4 + 3 * 67 ** + x * 67 ** 2 + 1 * 67 ** 1 + y * 67 ** 0
         num2 = 4 * x ** 3 + 9 * x ** 2 + y * x ** 1 + 6 * x ** 0
-        res.append(num1 + num2)
-print(len(set(res)))
+        res.add(num1 + num2)
+print(len(res))
