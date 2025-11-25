a2eed29ff7dc4a8d2fb7c342ecd23c40fbc37605
ner <ivanmironoved@gmail.com>
2025-11-25 19:16:35 +0200

    Д/З 24112025

--- Homework/12731.py
+++ Homework/12731.py
@@ -1,12 +1,9 @@
 from string import printable as al
 
-for i in range(0,17):
-    for x in al[:13]:
-        numc = int(f'9{x}AB', 13)
-    for x in al[:16]:
-        numd = int(f'{x}46C', 16)
-    for x in al[:15]:
-        numee = int(f'B7{x}15', 15)
-    num = numc + numd - numee
+for x in al[1:13]:
+    num1 = int(f'9{x}AB', 13)
+    num2 = int(f'{x}46C', 16)
+    num3 = int(f'B7{x}', 15)
+    num = num1 + num2 - num3
     if num % 14 == 0:
         print(x, num // 14)
