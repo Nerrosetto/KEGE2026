91b4fe77898e3ca2613e308f6abca9201d05c22b
Nerrosetto <ivanmironoved@gmail.com>
2025-12-16 19:49:03 +0200

    Д/З 16122025

--- Homework/13253.py
+++ Homework/13253.py
@@ -2,6 +2,7 @@ from itertools import product as per
 
 END = []
 DRA = []
+ee = []
 cnt = 0
 for i in set(per('КОНЕЦ', repeat=5)):
     i = ''.join(i)
@@ -9,4 +10,7 @@ for i in set(per('КОНЕЦ', repeat=5)):
 for i in set(per('ДРАКОН', repeat=5)):
     i = ''.join(i)
     DRA.append(i)
-print(len(set(END + DRA)))
+for i in set(per('КОН', repeat=5)):
+    i = ''.join(i)
+    ee.append(i)
+print(len(END) + len(DRA) - 2 * len(ee))
