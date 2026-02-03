72634c9342eb8ae39e78921b95a333eb8890c923
ner <ivanmironoved@gmail.com>
2026-02-03 19:44:47 +0200

    Дополнение к теории задания тип 25.

--- Task25/Theory.py
+++ Task25/Theory.py
@@ -41,3 +41,13 @@ for l in range(0, 4):
 
 for i in sorted(ans):
     print(*i)
+
+
+# Проверка чисел на простоту:
+def is_prime(num):
+    if num < 2:  # Избавляет от ошибок при отрицательных числах и 1.
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
