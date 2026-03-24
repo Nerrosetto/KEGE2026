ad2a9e3afb37d27785ef63f76eed7d866abdb145
ner <ivanmironoved@gmail.com>
2026-03-24 18:09:14 +0200

    Исправлено.

--- Task24/8510.py
+++ Task24/8510.py
@@ -1,4 +1,13 @@
-with open(r'C:\Users\Acer\PycharmProjects\KEGE20261\Task24\Files\24_8510.txt') as file:
+# with open(r'C:\Users\Acer\PycharmProjects\KEGE20261\Task24\Files\24_8510.txt') as file:
+#     data = file.readline()
+#
+# data = data.replace('O', 'P').replace('N', 'P')
+# while 'NN' in data:
+#     data = data.replace('NN', 'N N')
+#
+# print(max(len(i) for i in data.split()))
+
+with open(r'C:\Users\Ner\PycharmProjects\KEGE2026\Task24\Files\24_8510.txt') as file:
     data = file.readline()
 
 data = data.replace('O', 'P').replace('N', 'P')
