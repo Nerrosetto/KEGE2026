a4d8c7a38d0839dd67cf48be3a4778b466352f7c
ner <ivanmironoved@gmail.com>
2026-03-31 18:53:04 +0200

    Д/З.

--- Homework/18973.py
+++ Homework/18973.py
@@ -5,13 +5,10 @@ cnt = 0
 for i in pro(pri[:25], repeat=4):
     i = ''.join(i)
     a = []
-    u1 = False
-    u2 = False
     if i[0] != '0':
-        for t in i:
-            a += [str(pri.index(t))]
-        u1 = sum([a.count(q) for q in a if int(q) % 2 == 0]) >= 1
-        u2 = sum([int(i) > 15 for i in a]) > 2
-    if all((u1, u2)):
-        cnt += 1
+        a += [pri.index(t) for t in i]
+        u1 = sum([a.count(q) for q in a if q % 2 == 0]) >= 1
+        u2 = sum([i > 15 for i in a]) > 2
+        if all((u1, u2)):
+            cnt += 1
 print(cnt)
