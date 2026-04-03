4922c0ac821a04c3997bb1441c1a745ac7546fbb
Nerrosetto <ivanmironoved@gmail.com>
2026-04-03 18:39:15 +0200

    Теория тип 13.

--- Task13/Theory.py
+++ Task13/Theory.py
@@ -14,3 +14,18 @@ net_2 = ip_network('172.16.128.0/255.255.192.0', False)
 bin_ip = f'{int(ip):032b}'
 
 # Пример из N_17526 из kompege ^
+
+# Ip-адрес сети. Зарезервирован, не может быть выдан устройству
+network_address = net.network_address
+
+# Широковещательный адрес. Зарезервирован, не может быть выдан устройству
+broadcast_address = net.broadcast_address
+
+# Список всех узлов / хостов / ip-адреса устройств в текущей сети
+hosts = net.hosts()
+
+# Кол-во ip-адресов включая широковещательный и адрес сети
+num_addresses = net.num_addresses
+
+# Маска сети
+mask = net.netmask
