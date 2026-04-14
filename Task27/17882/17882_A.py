71a141cdeacd1ce0064f862cf14a6f127398a7e3
ner <ivanmironoved@gmail.com>
2026-04-14 19:18:20 +0200

    Тип 27.

---
+++ Task27/17882/17882_A.py
@@ -0,0 +1,21 @@
+from math import dist
+
+with open('../Files/27_A_17882.txt') as file:
+    dots = [list(map(float, i.split())) for i in file]
+
+
+def center(cluster):
+    res = []
+    for dot in cluster:
+        sum_dist = sum(dist(dot, d) for d in cluster)
+        res.append([sum_dist, dot])
+    return min(res)[1]
+
+
+cluster_1 = [dot for dot in dots if dot[1] < 3]
+cluster_2 = [dot for dot in dots if dot[1] > 3]
+
+center_1 = center(cluster_1)
+center_2 = center(cluster_2)
+print(f'x:{(center_1[0] + center_2[0]) / 2 * 10000}')
+print(f'y:{(center_1[1] + center_2[1]) / 2 * 10000}')
