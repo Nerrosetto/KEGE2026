80e3ed21c5cab416b739693e0c36c6330f7eca4d
Nerrosetto <ivanmironoved@gmail.com>
2026-05-22 19:39:26 +0200

    Д/З

--- Homework/18186/18186.py
+++ Homework/18186/18186.py
@@ -1,7 +1,17 @@
+from re import finditer
+
 with open(r'../18186/24_18186.txt') as file:
     data = file.readline()
 data1 = data
 
+# re:
+SSG = R'[^AE]{2}[AE]'
+pattern = rf'(?<={SSG}).*?(?={SSG})'  # ?<= - "обязательно в начале", ?= - "обязательно в конце"
+# ? отключает "жадность" регулярки.
+matches = [match.group() for match in finditer(pattern, data)]
+print(len(max(matches, key=len)) + 6)
+
+# zamena:
 for i in 'BCDFGH':
     data1 = data1.replace(i, '*')
 for i in 'AE':
@@ -16,6 +26,5 @@ for pos, i in enumerate(data1):
             pos1 = pos
         else:
             pos2 = pos
-    ans.append(len(data1[pos1:pos2 + 1]))
-
+    ans.append(len(data1[pos1:pos2 + 1]) + 6)
 print(max(ans))
